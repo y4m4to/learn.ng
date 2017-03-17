60bf7aeeed560ae24a00b99f3a5d00e5b8833a6b
Yamato Aizawa <yamato.aizawa@bizreach.co.jp>
2017-03-17 20:01:49 +0900

    y4m4to

--- src/components/number.button/number.button.component.ts
+++ src/components/number.button/number.button.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'calc-num-button',
   templateUrl: '../abstract.button/abstract.button.component.html',
   styleUrls: ['../abstract.button/abstract.button.component.scss']
 })
-export class NumberButtonComponent {}
+export class NumberButtonComponent {
+  @Input() public value: number;
+  @Input() public calc: any;
+
+  get text () : string {
+    return String(this.value);
+  }
+
+  public ngOnInit () : void {
+    console.log(this.text);
+  }
+
+  public onClick () : void {
+    this.calc.total += this.value;
+  }
+}
